c13ee28418833ec67058a0c4d150018dbca484b8
eryckavel <eryckavel@gmail.com>
2023-03-07 12:15:11 -0300

    inicio do curso

---
+++ realmeet/service/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.eryckavel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
